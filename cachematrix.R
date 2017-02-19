b4b3202095cf22553d992e16a80b322cc2656145
IreneSainz <ire.sainz.villalba@gmail.com>
2017-02-19 19:50:40 +0100

    IreneVSainz version
    from swirl_loop activity coursera.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#Creates a special "matrix" object 
+#that can cache its inverse.
+makeCacheMatrix <- function(x=matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  set_imatrix <- function(solve) m <<- solve
+  get_imatrix <- function() m
+  list(set = set, get = get,
+       set_imatrix = set_imatrix,
+       get_imatrix = get_imatrix)
 }
 
-
-## Write a short comment describing this function
-
+#This function computes the inverse of the special "matrix" 
+#returned by makeCacheMatrix above.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$get_imatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$set_imatrix(m)
+  m
 }
